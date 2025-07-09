a523163eda01899ccae62db198734dc2632da780
Rvtar <shinboon.boon@gmail.com>
2025-07-09 20:47:25 +0700

    Update jack-rare.test.ts

--- tests/unit/game/shifttech/hermits/jack-rare.test.ts
+++ tests/unit/game/shifttech/hermits/jack-rare.test.ts
@@ -2,39 +2,39 @@ import {describe, expect, test} from '@jest/globals'
 import BuilderItem from 'common/cards/items/builder-common'
 import BuilderDoubleItem from 'common/cards/items/builder-rare'
 import JackRare from 'common/cards/shifttech/hermits/jack-rare'
-import {attack, endTurn, playCardFromHand, testGame} from '../../utils'
+import {testGame} from '../../utils'
 
 describe('Test Jack Rare', () => {
-	test('Test Jack Rare Base Functionality', () => {
-		testGame(
+	test('Test Jack Rare Base Functionality', async () => {
+		await testGame(
 			{
 				playerOneDeck: [JackRare],
 				playerTwoDeck: [JackRare, BuilderItem, BuilderItem],
-				saga: function* (game) {
-					yield* playCardFromHand(game, JackRare, 'hermit', 0)
-					yield* endTurn(game)
+				testGame: async (test, game) => {
+					await test.playCardFromHand(JackRare, 'hermit', 0)
+					await test.endTurn()
 
-					yield* playCardFromHand(game, JackRare, 'hermit', 0)
-					yield* playCardFromHand(game, BuilderItem, 'item', 0, 0)
-					yield* attack(game, 'secondary')
-					yield* playCardFromHand(game, BuilderItem, 'item', 0, 1)
+					await test.playCardFromHand(JackRare, 'hermit', 0)
+					await test.playCardFromHand(BuilderItem, 'item', 0, 0)
+					await test.attack('secondary')
+					await test.playCardFromHand(BuilderItem, 'item', 0, 1)
 				},
 			},
 			{startWithAllCards: true, noItemRequirements: true, forceCoinFlip: true},
 		)
 	})
-	test('Test Jack Rare Double Item First Condition', () => {
-		testGame(
+	test('Test Jack Rare Double Item First Condition', async () => {
+		await testGame(
 			{
 				playerOneDeck: [JackRare],
 				playerTwoDeck: [JackRare, BuilderDoubleItem],
-				saga: function* (game) {
-					yield* playCardFromHand(game, JackRare, 'hermit', 0)
-					yield* endTurn(game)
+				testGame: async (test, game) => {
+					await test.playCardFromHand(JackRare, 'hermit', 0)
+					await test.endTurn()
 
-					yield* playCardFromHand(game, JackRare, 'hermit', 0)
-					yield* playCardFromHand(game, BuilderDoubleItem, 'item', 0, 0)
-					yield* attack(game, 'secondary')
+					await test.playCardFromHand(JackRare, 'hermit', 0)
+					await test.playCardFromHand(BuilderDoubleItem, 'item', 0, 0)
+					await test.attack('secondary')
 					expect(game.state.turn.availableActions).not.toContain(
 						'PLAY_ITEM_CARD',
 					)
@@ -43,20 +43,20 @@ describe('Test Jack Rare', () => {
 			{startWithAllCards: true, noItemRequirements: true, forceCoinFlip: true},
 		)
 	})
-	test('Test Jack Rare Double Item Second Condition', () => {
-		testGame(
+	test('Test Jack Rare Double Item Second Condition', async () => {
+		await testGame(
 			{
 				playerOneDeck: [JackRare],
 				playerTwoDeck: [JackRare, BuilderDoubleItem],
-				saga: function* (game) {
-					yield* playCardFromHand(game, JackRare, 'hermit', 0)
-					yield* endTurn(game)
+				testGame: async (test, game) => {
+					await test.playCardFromHand(JackRare, 'hermit', 0)
+					await test.endTurn()
 
-					yield* playCardFromHand(game, JackRare, 'hermit', 0)
-					yield* attack(game, 'secondary')
+					await test.playCardFromHand(JackRare, 'hermit', 0)
+					await test.attack('secondary')
 					expect(game.currentPlayer.getCardsCanBePlacedIn()).toStrictEqual([])
 					expect(
-						playCardFromHand(game, BuilderDoubleItem, 'item', 0, 0),
+						await test.playCardFromHand(BuilderDoubleItem, 'item', 0, 0),
 					).toThrow('You cannot play cards that are in frozen slots')
 				},
 			},
