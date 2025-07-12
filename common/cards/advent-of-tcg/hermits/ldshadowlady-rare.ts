5360f9c688aae8e644f90540d19b5c9330a07c67
Rvtar <shinboon.boon@gmail.com>
2025-07-13 01:59:32 +0700

    Update ldshadowlady-rare.ts

--- common/cards/advent-of-tcg/hermits/ldshadowlady-rare.ts
+++ common/cards/advent-of-tcg/hermits/ldshadowlady-rare.ts
@@ -19,8 +19,6 @@ const LDShadowLadyRare: Hermit = {
 	expansion: 'mcyt',
 	palette: 'advent_of_tcg',
 	background: 'advent_of_tcg',
-	set: ['Advent'],
-	update: 3
 	rarity: 'rare',
 	tokens: 2,
 	type: ['terraform'],
