1b393058d05d79eb2d0bcfdeb12bb9de9dfee3b1
Rvtar <shinboon.boon@gmail.com>
2025-08-12 00:56:29 +0700

    InterimNameCommon Generation

--- common/cards/temp/index.ts
+++ common/cards/temp/index.ts
@@ -1,6 +1,10 @@
 import {Card} from '../types'
 import TinFoilChefMythic from './tinfoilchef-mythic'
+import InterimNameCommon from './interimname-common'
 
-const hermitCardClasses: Array<Card> = [TinFoilChefMythic]
+const hermitCardClasses: Array<Card> = [
+	TinFoilChefMythic,
+	InterimNameCommon
+]
 
 export default hermitCardClasses
---
+++ common/cards/temp/interimname-common.ts
@@ -0,0 +1,28 @@
+import {hermit} from '../defaults'
+import {Hermit} from '../types'
+
+const InterimNameCommon: Hermit = {
+	...hermit,
+	id: 'interimname_common',
+	numericId: 131,
+	name: 'InterimName',
+	expansion: 'shifttech',
+	rarity: 'common',
+	tokens: 0,
+	type: ['redstone'],
+	health: 290,
+	primary: {
+		name: 'Due South',
+		cost: ['redstone'],
+		damage: 60,
+		power: null,
+	},
+	secondary: {
+		name: 'New Story',
+		cost: ['redstone', 'redstone'],
+		damage: 80,
+		power: null,
+	},
+}
+
+export default InterimNameCommon
