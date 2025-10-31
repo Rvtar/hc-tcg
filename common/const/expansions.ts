a6ed424af04a71ffdecd3bc88fb4ac34f36b0335
Rvtar <shinboon.boon@gmail.com>
2025-10-31 01:32:46 -0700

    Update expansions.ts

--- common/const/expansions.ts
+++ common/const/expansions.ts
@@ -11,32 +11,10 @@ export type ExpansionT =
 	| 'beds'
 	| 'boss'
 	// Scuffed
-	| 'hc_plus'
-	| 'hcx'
-	| 'minecraft'
-	| 'shifttech'
-	| 'btc'
-	| 'discord'
-	| 'mcyt'
-	| 'baseball'
-	| 'ninjago'
-	| 'television'
-	| 'fortnite'
-	| 'survivor'
-	| 'holidays'
-	| 'new_vegas'
-	| 'youtube'
-	| 'marvel'
-	| 'decked_out'
-	| 'artifake'
-	| 'create'
-	| 'his_fig'
-	| 'modesto'
-	| 'vg_legends'
-	| 'touhou'
-	| 'villager_news'
-	| 'gravity_falls'
-	| 'terraria'
+	| 'bedrock_gang'
+	| 'tales_of_bedrock_gang'
+	| 'plain_pastures'
+	| 'nightmare_on_mojang'
 
 export type ExpansionDict = {
 	readonly [Expansion in ExpansionT]: ExpansionInfo
@@ -45,33 +23,11 @@ export type ExpansionDict = {
 export const EXPANSIONS: ExpansionDict = {
 	item: {name: 'Item', disabled: false},
 	default: {name: 'Hermitcraft', disabled: false},
-	hcx: {name: 'HermitcraftX', disabled: false},
-	hc_plus: {name: 'HC+', disabled: false},
 	alter_egos: {name: 'Alter Egos', disabled: false},
 	beds: {name: 'Beds', disabled: true}, //@ScTODO Integrate into Minecraft
 	boss: {name: 'Boss', disabled: true},
-	minecraft: {name: 'Minecraft', disabled: false},
-	shifttech: {name: 'Shifttech', disabled: false},
-	btc: {name: 'Beyond the Cosmos', disabled: false},
-	discord: {name: 'HC TCG Discord', disabled: false},
-	mcyt: {name: 'MCYT', disabled: false},
-	baseball: {name: 'Baseball', disabled: false},
-	ninjago: {name: 'Ninjago', disabled: false},
-	television: {name: 'Television', disabled: false},
-	fortnite: {name: 'Fortnite', disabled: false},
-	survivor: {name: 'Survivor', disabled: false},
-	holidays: {name: 'Holidays', disabled: false},
-	new_vegas: {name: 'New Vegas', disabled: false},
-	youtube: {name: 'Youtube', disabled: false},
-	marvel: {name: 'Marvel', disabled: false},
-	decked_out: {name: 'Decked Out', disabled: false},
-	artifake: {name: 'Artifake', disabled: false},
-	create: {name: 'Create', disabled: false},
-	his_fig: {name: 'Historical Figures', disabled: false},
-	modesto: {name: 'Modesto', disabled: false},
-	vg_legends: {name: 'Video Game Legends', disabled: false},
-	touhou: {name: 'Touhou Project', disabled: false},
-	villager_news: {name: 'Villager News', disabled: false},
-	gravity_falls: {name: 'Gravity Falls', disabled: false},
-	terraria: {name: 'Terraria', disabled: false},
+	bedrock_gang: {name: 'Bedrock Gang', disabled: false},
+	tales_of_bedrock_gang: {name: 'Tales of Bedrock Gang', disabled: false},
+	plain_pastures: {name: 'Plain Pastures', disabled: false},
+	nightmare_on_mojang: {name: 'Nightmare On Mojang', disabled: false},
 }
